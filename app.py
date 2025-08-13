b54a3999db6ebf13e84d2e83fe830117256048ab
isabella2311 <65619011+isabella2311@users.noreply.github.com>
2025-08-13 20:21:24 +0200

    app.py

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+app.py
+db.py
+models.py
+requirements.txt
+.streamlit/config.toml
+pages/   (todas las páginas dentro)
 import os, datetime as dt, pandas as pd, streamlit as st, plotly.express as px
 from db import init_db, SessionLocal
 from models import Transaction, Budget, Goal, Debt, Investment, Account
